1105101d901c11495511351ba9ac8d888df1abcc
Maksim Maksimov <maximebayer@gmail.com>
2025-01-07 15:49:31 -0500

    add 3 API tests

--- test_api.py
+++ test_api.py
@@ -1,20 +1,31 @@
 import requests as r
 import unittest
+import time
+
 
 from requests import session
-
 # Test API endpoint
 
 #SWAGGER dos: https://todo.pixegami.io/docs
 
 ENDPOINT_1 = 'https://todo.pixegami.io'
-ENDPOINT_2 = 'https://codeforces.com/api/get'
+
 
 def test_api_get_1():
+    start_time = time.time()  # Start timing
     resp = r.get(ENDPOINT_1)
-    assert resp.status_code == 200
+    acceptable_time_seconds = 3  # Define the acceptable time in seconds
+    end_time = time.time()  # End timing
+
+    execution_time = end_time - start_time
+    assert execution_time <= acceptable_time_seconds, (
+        f"API call took {execution_time:.4f} seconds, which exceeds the limit of {acceptable_time_seconds} seconds"
+    )
+
+    print(f"API call completed in {execution_time:.4f} seconds")
     assert resp.json() == {'message': 'Hello World from Todo API'}
     assert resp.headers['Content-Type'] == 'application/json'
+    assert resp.status_code == 200
 
     # second way to assert by using unittest
     unittest.TestCase().assertEqual(resp.status_code, 200)
@@ -25,19 +36,17 @@ def test_can_create_task_2():
                "user_id": "dummy",
                "task_id": "1234567",
                "is_done": False}
-    resp = r.put(f'{ENDPOINT_1}/create-task', json=payload)
-    print(resp.json())
+    create_task = r.put(f'{ENDPOINT_1}/create-task', json=payload)
     global task_id
-    task_id = resp.json()['task']['task_id']
+    task_id = create_task.json()['task']['task_id']
     print(task_id)
-    assert resp.status_code == 200
-
+    print(create_task.json())
+    assert create_task.status_code == 200
+    assert create_task.json()['task']['content'] == payload['content']
+    assert create_task.json()['task']['user_id'] == payload['user_id']
 def test_can_get_task_3():
-    resp = r.get(ENDPOINT_1 + f'get-task/{task_id}')
+    print('Test3')
+    resp = r.get(ENDPOINT_1 + f'/get-task/{task_id}')
     print(resp.json())
     assert resp.status_code == 200
 
-# def test_can_get_task_4():
-#     resp = r.get(f'{ENDPOINT_1} + 'get-'get/task_7e086eaf6d7a49b5b0e545517ffc72a8')
-#     print(resp.json())
-#     assert resp.status_code == 200
